992886d7a7c59fbca9e3300bd53a510bf33ef0f3
ViniMagaa <vinisantos2008vs@gmail.com>
2025-09-04 21:46:02 -0300

    feat: add sign-out function on admin panel

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,3 +1,31 @@
+import { SignOutButton } from "@/components/sign-out-button";
+import { db } from "@/lib/prisma";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
 export default async function Dashboard() {
-  return <h1>Admin</h1>;
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return redirect("/dashboard");
+
+  const userData = await db.user.findUnique({ where: { id: user.id } });
+
+  return (
+    <div className="w-full space-y-4 p-4">
+      <div className="flex w-full flex-wrap justify-between gap-4">
+        <div>
+          <h1 className="text-4xl font-bold">Olá, {userData?.name}!</h1>
+          <p className="text-muted-foreground">
+            Você está no painel administrativo.
+          </p>
+        </div>
+
+        <SignOutButton />
+      </div>
+    </div>
+  );
 }
